589085107dd65577511cb6b1d537de1d59e170ff
xelaint <xelaint@gmail.com>
2016-04-09 22:08:29 -0400

    set up part 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var browserify = require('gulp-browserify');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -17,8 +18,16 @@ gulp.task('serve', ['sass'], function() {
 gulp.task('sass', function() {
     return gulp.src("sass/app.scss")
         .pipe(sass())
-        .pipe(gulp.dest("css"))
+        .pipe(gulp.dest("./build/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
+
+gulp.task('scripts', function() {
+	// Single entry point to browserify 
+	gulp.src('js/app.js')
+		.pipe(browserify({
+		}))
+		.pipe(gulp.dest('./build/js'))
+});
