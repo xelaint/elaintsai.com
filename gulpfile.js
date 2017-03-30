04c05cdbfccb09ed25639fcc0ea5c93738e26203
Damien Retzinger <damienwebdev@gmail.com>
2017-03-29 21:59:38 -0400

    fixing up some minor build files

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var browserify = require('gulp-browserify');
 
+var outputFolder = "docs";
+var srcFolder = "src/";
+
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
@@ -10,24 +13,38 @@ gulp.task('serve', ['sass'], function() {
         server: "."
     });
 
-    gulp.watch("sass/components/*.scss", ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch(srcFolder + "sass/components/*.scss", ['sass']);
+    gulp.watch(srcFolder + "**/*.html").on('change', browserSync.reload);
 });
+
+gulp.task('build', ['html','sass','scripts', 'assets']);
+
+gulp.task('assets', function(){
+  return gulp.src(srcFolder + "assets/**/*")
+      .pipe(gulp.dest( outputFolder + "/assets"))
+})
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("sass/app.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("./build/css"))
+    return gulp.src(srcFolder + "sass/app.scss")
+        .pipe(sass({
+          includePaths: './node_modules/bootstrap-sass/assets/stylesheets'
+        }))
+        .pipe(gulp.dest( outputFolder + "/css"))
         .pipe(browserSync.stream());
+});
+
+gulp.task('html', function() {
+  return gulp.src(srcFolder + "**/*.html")
+      .pipe(gulp.dest( outputFolder + "/html"));
 });
 
 gulp.task('default', ['serve']);
 
 gulp.task('scripts', function() {
-	// Single entry point to browserify 
-	gulp.src('js/app.js')
+	// Single entry point to browserify
+	gulp.src(srcFolder + 'js/app.js')
 		.pipe(browserify({
 		}))
-		.pipe(gulp.dest('./build/js'))
+		.pipe(gulp.dest(outputFolder +'/js'))
 });
