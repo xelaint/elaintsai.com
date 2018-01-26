1ebf0d83653835bd99e3699aac5792b2dbae5264
xelaint <xelaint@gmail.com>
2018-01-26 12:44:21 +0800

    edits

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,16 @@
 window.$ = window.jQuery = require('jquery');
 require("../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js");
+
+/*
+	Smooth scroll functionality for anchor links (animates the scroll
+	rather than a sudden jump in the page)
+*/
+
+$('.js-anchor-link').click(function(e){
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    if(target.length){
+      var scrollTo = target.offset().top;
+      $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
+    }
+  });
