3176074777ed521b04c1fab5869b7665072cb119
Damien Retzinger <damienwebdev@gmail.com>
2018-01-28 13:16:43 -0500

    changing untar script

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -x
 
-cd /var/www/vhosts/elaintsai.com && \ 
-rm -rf public_html/* && \
+cd $REMOTE_APP_DIR && \ 
+rm -rf public_html && \
+mkdir public_html && \
 tar zxvf package.tgz -C public_html && \
 rm package.tgz
